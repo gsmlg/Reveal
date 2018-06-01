2ad93833d321399f4448bfda3844d315b7d8161a
gaoshiming <gaoshiming@zdns.cn>
2018-06-01 09:31:42 +0800

    virtual-dom

---
+++ virtual-dom/src/config/Dev.js
@@ -0,0 +1,7 @@
+import baseConfig from './Base';
+
+const config = {
+  appEnv: 'dev',
+};
+
+export default Object.freeze(Object.assign({}, baseConfig, config));
---
+++ virtual-dom/src/slides/index.js
@@ -0,0 +1,33 @@
+
+let slides = [];
+
+slides.push(`
+<section>
+	<h1>Virtual DOM</h1>
+</section>
+`);
+
+let markdown = `
+<section data-markdown>
+	<textarea data-template>
+## 虚拟DOM技术应用
+
+
+
+	</textarea>
+</section>
+`;
+
+slides.push(markdown);
+
+
+let note = `
+<section>
+
+
+
+</section>
+`;
+slides.push(note);
+
+export default slides;
